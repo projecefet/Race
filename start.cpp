b61813ae5e0800554e229e9a861b639ec8c26672
★ ࣪ ˖ MD!! ˊˎ <134858819+wisthg@users.noreply.github.com>
2023-06-19 17:36:57 -0300

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -1,13 +1,8 @@
 // CODIGO DA TELA INICIAL
 
-#include "race.hpp"
+#include <SFML/Graphics.hpp>
 
-int main() {
-	bool click, keygame;
-	click = false;
-
-	sf::RenderWindow window(sf::VideoMode(960, 540), "MARIO KART: Super Circuit",
-			sf::Style::Titlebar | sf::Style::Close);
+void menu(sf::RenderWindow *window, int *janelaControle) {
 	sf::Texture imagem;
 	sf::Sprite fundo;
 	sf::RectangleShape mouseplay;
@@ -16,44 +11,18 @@ int main() {
 
 	imagem.loadFromFile("assets/menu.png");
 	fundo.setTexture(imagem);
-	mouseplay.setSize(sf::Vector2f(440, 287));
-	mouseplay.setPosition(356, 115);
+	mouseplay.setSize(sf::Vector2f(365, 145));
+	mouseplay.setPosition(429, 288);
 	sf::Image image = sf::Image { };
 
-	image.loadFromFile("assets/icon.png");
-	window.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
+	leituramouse = sf::Mouse::getPosition(*window);
+	cordenadatual = window->mapPixelToCoords(leituramouse);
 
-	while (window.isOpen()) {
-		sf::Event event;
-		leituramouse = sf::Mouse::getPosition(window);
-		cordenadatual = window.mapPixelToCoords(leituramouse);
-
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && click == false) {
-				click = true;
-			}
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+		if (mouseplay.getGlobalBounds().contains(cordenadatual)) {
+			*janelaControle = 1;
 		}
-
-		if (click == true) {
-			if (mouseplay.getGlobalBounds().contains(cordenadatual)) {
-				keygame = true;
-			}
-			click = false;
-		}
-
-		if (keygame == true) {
-
-			jogo(&window);
-		}
-
-		window.clear();
-		window.draw(fundo);
-
-		window.display();
 	}
 
-	return 0;
+	window->draw(fundo);
 }
