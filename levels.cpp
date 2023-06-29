f78c5d5a52d5ccaa6401e5371e2dc4b3c4920950
★ ࣪ ˖ MD!! ˊˎ <134858819+wisthg@users.noreply.github.com>
2023-06-29 17:20:50 -0300

    Update levels.cpp

--- levels.cpp
+++ levels.cpp
@@ -4,7 +4,7 @@
 #include "levels.hpp"
 
 Levels::Levels () {
-	this->backgroundTexture.loadFromFile("assets/nivel/fundolevels.png");
+	this->backgroundTexture.loadFromFile("assets/levels/fundolevels.png");
 	this->backgroundSprite.setTexture(this->backgroundTexture);
 }
 Levels::~Levels () {}
@@ -15,15 +15,15 @@ void Levels::render(sf::RenderWindow *window) {
 
 // Star Button
 StarButton::StarButton (int stars, sf::Vector2f initialPos, int btnNum) {
-	this->buttonTexture.loadFromFile("assets/nivel/" + std::to_string(stars) + "star.png");
+	this->buttonTexture.loadFromFile("assets/levels/" + std::to_string(stars) + "star.png");
 	this->buttonSprite.setTexture(this->buttonTexture);
-	this->buttonSprite.setScale(sf::Vector2f(0.75, 0.75));
+	this->buttonSprite.setScale(sf::Vector2f(0.05, 0.05));
 
-	this->phaseNumTexture.loadFromFile("assets/nivel/" + std::to_string(btnNum) + ".png");
+	this->phaseNumTexture.loadFromFile("assets/levels/" + std::to_string(btnNum) + ".png");
 	this->phaseNumSprite.setTexture(this->phaseNumTexture);
 
 	this->phaseNumSprite.setPosition(initialPos);
-	this->phaseNumSprite.setScale(sf::Vector2f(0.19, 0.19));
+	this->phaseNumSprite.setScale(sf::Vector2f(0.09, 0.09));
 
 	this->buttonSprite.setPosition(initialPos);
 }
