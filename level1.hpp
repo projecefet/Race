f7be77e5b76ac815795cf4d6a2f2a8129e02e3be
★ ࣪ ˖ MD!! ˊˎ <134858819+wisthg@users.noreply.github.com>
2023-06-29 17:24:01 -0300

    Add files via upload

---
+++ level1.hpp
@@ -0,0 +1,17 @@
+/*
+ * level1.hpp
+ *
+ *  Created on: 29 de jun. de 2023
+ *      Author: 2306m
+ */
+
+#ifndef LEVEL1_HPP_
+#define LEVEL1_HPP_
+
+#include "race.hpp"
+
+int level1 (sf::RenderWindow *window);
+
+
+
+#endif /* LEVEL1_HPP_ */
