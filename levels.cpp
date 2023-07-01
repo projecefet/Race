fec562dca9f010bedeb2bc0c68264506806a091d
★ ࣪ ˖ MD!! ˊˎ <134858819+wisthg@users.noreply.github.com>
2023-06-30 23:05:55 -0300

    Update levels.cpp

--- levels.cpp
+++ levels.cpp
@@ -1,24 +1,27 @@
-// TELA DOS NIVEIS  - CPP
+// TELA DE NIVEIS - CPP
 
 #include "levels.hpp"
 
-Levels::Levels () {
+Levels::Levels() {
 	this->backgroundTexture.loadFromFile("assets/levels/fundolevels.png");
 	this->backgroundSprite.setTexture(this->backgroundTexture);
 }
-Levels::~Levels () {}
+Levels::~Levels() {
+}
 
 void Levels::render(sf::RenderWindow *window) {
 	window->draw(this->backgroundSprite);
 }
 
 // Star Button
-StarButton::StarButton (int stars, sf::Vector2f initialPos, int btnNum) {
-	this->buttonTexture.loadFromFile("assets/levels/" + std::to_string(stars) + "star.png");
+StarButton::StarButton(int stars, sf::Vector2f initialPos, int btnNum) {
+	this->buttonTexture.loadFromFile(
+			"assets/levels/" + std::to_string(stars) + "star.png");
 	this->buttonSprite.setTexture(this->buttonTexture);
 	this->buttonSprite.setScale(sf::Vector2f(0.20, 0.20));
 
-	this->phaseNumTexture.loadFromFile("assets/levels/" + std::to_string(btnNum) + ".png");
+	this->phaseNumTexture.loadFromFile(
+			"assets/levels/" + std::to_string(btnNum) + ".png");
 	this->phaseNumSprite.setTexture(this->phaseNumTexture);
 
 	this->phaseNumSprite.setPosition(initialPos);
@@ -27,9 +30,27 @@ StarButton::StarButton (int stars, sf::Vector2f initialPos, int btnNum) {
 	this->buttonSprite.setPosition(initialPos);
 }
 
-StarButton::~StarButton () {}
+StarButton::~StarButton() {
+}
 
 void StarButton::render(sf::RenderWindow *window) {
 	window->draw(this->buttonSprite);
 	window->draw(this->phaseNumSprite);
+}
+
+sf::FloatRect StarButton::getRect() {
+	return this->buttonSprite.getGlobalBounds();
+}
+
+bool StarButton::didGetClicked(sf::RenderWindow *window) {
+	sf::Vector2i mousePosI = sf::Mouse::getPosition(*window);
+	sf::Vector2f coord = window->mapPixelToCoords(mousePosI);
+
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+		if (this->buttonSprite.getGlobalBounds().contains(coord)) {
+			return true;
+		}
+	}
+
+	return false;
 }
