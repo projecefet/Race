b7894e2a1194578c82753bf9e0fab32b2e78aeb0
★ ࣪ ˖ MD!! ˊˎ <134858819+wisthg@users.noreply.github.com>
2023-07-04 15:16:41 -0300

    Update level1.hpp

--- level1.hpp
+++ level1.hpp
@@ -5,6 +5,7 @@
 
 #include "race.hpp"
 
-int level1 (sf::RenderWindow *window);
+bool level1 (sf::RenderWindow *window, int *frameCounter_Player1,
+		int *frameCounter_Player2, int* janelaControle);
 
-#endif 
+#endif
