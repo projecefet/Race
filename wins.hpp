40376b6f80a307f8a1e43ae6f663f459b24c803b
★ ࣪ ˖ MD!! ˊˎ <134858819+wisthg@users.noreply.github.com>
2023-07-04 10:15:19 -0300

    Update wins.hpp

--- wins.hpp
+++ wins.hpp
@@ -4,7 +4,6 @@
 using namespace std;
 using namespace sf;
 
-
 void telabowser(sf::RenderWindow *window, int *janelaControle){
 	sf::RectangleShape mouseplay;
 	sf::Vector2i leituramouse;
@@ -49,7 +48,35 @@ void telabowser(sf::RenderWindow *window, int *janelaControle){
 
 }
 
+//CODIGO DA TELA DO MARIO
+void telamario(sf::RenderWindow *window, int *janelaControle){
+	sf::RectangleShape mouseplay;
+	sf::Vector2i leituramouse;
+	sf::Vector2f cordenadatual;
+    sf::Texture texture;
+    sf::Sprite image;
+
+    texture.loadFromFile("assets/mario.png");
+    image.setTexture(texture);
+    mouseplay.setSize(sf::Vector2f(242, 72));
+    mouseplay.setPosition(64, 369);
+    Image image = sf::Image { };
 
 
-//codigo da tela do mario
+    //sf::FloatRect botaoArea(64, 369, 242, 72);
+    //sf::RectangleShape butao(sf::Vector2f(botaoArea.width, botaoArea.height)); //botao branco
 
+    //butao.setPosition(botaoArea.left, botaoArea.top);
+    //butao.setFillColor(sf::Color::White);
+
+    leituramouse = Mouse::getPosition(*window);
+    cordenadatual = window->mapPixelToCoords(leituramouse);
+
+     if(Mouse::isButtonPressed(Mouse::Left)){
+    	 if (mouseplay.getGlobalBounds().contains(cordenadatual)) {
+    		 *janelaControle = 1;
+         }
+     }
+     window->draw(image);
+
+}
