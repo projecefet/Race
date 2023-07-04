833a442b5b76d28e6a9fcc16c41a23457b4dbee7
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-07-04 19:58:08 -0300

    Update level1.hpp

--- level1.hpp
+++ level1.hpp
@@ -5,7 +5,7 @@
 
 #include "race.hpp"
 
-bool level1 (sf::RenderWindow *window, int *frameCounter_Player1,
-		int *frameCounter_Player2, int* janelaControle);
+bool level1(sf::RenderWindow *window, int *frameCounter_P1_level1,
+		int *frameCounter_P2_level1, Coins *coins, int (&coinsLeft)[10] ,int* janelaControle);
 
 #endif
