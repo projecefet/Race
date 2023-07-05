a434e82e770643dbae52e01ec095b58b7bd0c1d3
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-07-04 22:30:44 -0300

    Create level2.hpp

---
+++ level2.hpp
@@ -0,0 +1,11 @@
+// SEGUNDA FASE - HPP
+
+#ifndef LEVEL2_HPP_
+#define LEVEL2_HPP_
+
+#include "race.hpp"
+
+bool level2(sf::RenderWindow *window, int *frameCounter_P1_level1,
+		int *frameCounter_P2_level1, Coins *coins, int (&coinsLeft)[10] ,int* janelaControle);
+
+#endif
