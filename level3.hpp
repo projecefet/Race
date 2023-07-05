451b57d0220f2ded27d53a80b8dc5f52c57d997e
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-07-04 22:31:39 -0300

    Create level3.hpp

---
+++ level3.hpp
@@ -0,0 +1,14 @@
+
+// LEVEL 3 HPP
+
+#ifndef LEVEL3_HPP_
+#define LEVEL3_HPP_
+
+
+#include "race.hpp"
+
+bool level3(sf::RenderWindow *window, int *frameCounter_P1_level1,
+		int *frameCounter_P2_level1, Coins *coins, int (&coinsLeft)[10] ,int* janelaControle);
+
+
+#endif /* LEVEL3_HPP_ */
